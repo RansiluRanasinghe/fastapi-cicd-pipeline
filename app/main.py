c35369dde9b85e4a5d3d512e9309f3980445ff16
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-21 11:49:28 +0530

    feat: define the get_prediction for /predict Dummy ML prediction endpoint

--- app/main.py
+++ app/main.py
@@ -10,3 +10,16 @@ app = FastAPI(
 @app.get("/health", status_code=200)
 def health_check():
     return {"status": "healthy", "environment": "production"}
+
+@app.post("/predict", response_model=PredictionOutput, status_code=200)
+def get_prediction(payload: PredictionInput):
+
+    score = payload.feature_one * 0.4 + payload.feature_two * 0.6
+    prediction_label = 1 if score >= 0.5 else 0
+
+    probability_value = min(max(abs(score), 0.0), 1.0)
+
+    return {
+        "prediction": prediction_label,
+        "probability": round(probability_value, 2)
+    }
