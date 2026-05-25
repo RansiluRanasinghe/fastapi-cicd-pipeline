2439fe492b1e3cafc84697d8f44b9dd017b5544b
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-25 11:15:50 +0530

    feat: define the test_prediction_endpoint_negative_class to test the negative class from /prediction endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -21,3 +21,17 @@ def test_prediction_endpoint_positive_class():
     data = response.json()
     assert data["prediction"] == 1
     assert "probability" in data
+
+def test_prediction_endpoint_negative_class():
+
+    test_payload = {
+        "feature_one": 0.1,
+        "feature_two": 0.2
+    }
+    
+    response = client.post("/predict", json=test_payload)
+    assert response.status_code == 200
+    
+    data = response.json()
+    assert data["prediction"] == 0
+    assert data["probability"] >= 0.0
