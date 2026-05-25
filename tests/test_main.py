9dfbb2974d9af49ebd1f9e6d71499a0dd96b8532
RansiluRanasinghe <159747382+RansiluRanasinghe@users.noreply.github.com>
2026-05-25 11:13:24 +0530

    feat: define the test_prediction_endpoint_positive_class to test the positive class from /prediction endpoint

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,3 +8,16 @@ def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy", "environment": "production"}
+
+def test_prediction_endpoint_positive_class():
+
+    test_payload = {
+        "feature_one": 0.8,
+        "feature_two": 0.7
+    }
+    response = client.post("/predict", json=test_payload)
+    assert response.status_code == 200
+    
+    data = response.json()
+    assert data["prediction"] == 1
+    assert "probability" in data
